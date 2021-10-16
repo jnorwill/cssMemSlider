32e7ef537feea3330df89a73c87ef71e658390c6
Julia <julianorwill@gmail.com>
2021-10-17 02:33:38 +0300

    refactor: add default control(Sun, Nov 17, 2021 2:32 AM)

--- cssMemSlider/script.js
+++ cssMemSlider/script.js
@@ -2,14 +2,15 @@ const slidContainer = document.querySelector('.slider__container')
 const controlContainer = document.querySelector('.pagination')
 const textContainer = document.querySelector('.text-container__text')
 const textAll = ['Memes should have been attached here', 'Drink coffee in any situation', 'O-o, now someone will steal the shaggy', 'Purple guy with a weird smile']
+
 controlContainer.addEventListener('click', (event) => {
   const n = event.target.dataset.control
+  const controlFocus = document.querySelector('.pagination__control_focus')
+
   slidContainer.style.transform = `translateX(-${(n - 1) * 100}%)`
 
-  const controlFocus = document.querySelector('.pagination__control_focus')
-  if (controlFocus) {
-    controlFocus.classList.remove('pagination__control_focus')
-  }
+  controlFocus.classList.remove('pagination__control_focus')
+
   if (event.target.classList.contains('pagination__control-container')) {
     console.log(event.target.firstElementChild)
     event.target.firstElementChild.classList.add('pagination__control_focus')
@@ -18,14 +19,14 @@ controlContainer.addEventListener('click', (event) => {
   }
 
   textContainer.style.transition = '0s'
-    textContainer.innerHTML = textAll[n - 1]
-    textContainer.style.transform = 'translateX(100px)'
-    textContainer.style.opacity = '0'
+  textContainer.innerHTML = textAll[n - 1]
+  textContainer.style.transform = 'translateX(100px)'
+  textContainer.style.opacity = '0'
 
-    setTimeout(() => {
-      textContainer.style.transition = '0.2s'
-      textContainer.style.transform = 'translateX(0)'
-      textContainer.style.opacity = '1'
-    })
+  setTimeout(() => {
+    textContainer.style.transition = '0.2s'
+    textContainer.style.transform = 'translateX(0)'
+    textContainer.style.opacity = '1'
+  })
 })
 
